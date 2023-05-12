f565dcdfa619a859599da131beeeb8084660b74c
Cedric Mailleux <cmailleux@jahia.com>
2023-05-12 11:50:52 -0400

    QA-14831: fix pom.xml and JITClassLoader to compile with JDK 1.8

--- src/main/java/org/mvel2/util/JITClassLoader.java
+++ src/main/java/org/mvel2/util/JITClassLoader.java
@@ -16,40 +16,14 @@
  * limitations under the License.
  */
 
-package org.mvel2.util;
+ package org.mvel2.util;
 
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
-
-public class JITClassLoader extends ClassLoader implements MVELClassLoader {
-  private static boolean sunJVM;
-  private static Object sunUnsafe;
-
-  static {
-    try {
-      Field f = Unsafe.class.getDeclaredField("theUnsafe");
-      f.setAccessible(true);
-      sunUnsafe = f.get(null);
-      sunJVM = true;
-    }
-    catch (Throwable t) {
-      //      t.printStackTrace();
-      sunJVM = false;
-    }
-  }
-
-
-  public JITClassLoader(ClassLoader classLoader) {
-    super(classLoader);
-  }
-
-  public Class<?> defineClassX(String className, byte[] b, int off, int len) {
-    if (sunJVM) {
-      return ((Unsafe) sunUnsafe).defineClass(className, b, off, len);
-    }
-    else {
-      return super.defineClass(className, b, off, len);
-    }
-  }
-}
+ public class JITClassLoader extends ClassLoader implements MVELClassLoader {
+   public JITClassLoader(ClassLoader classLoader) {
+     super(classLoader);
+   }
+ 
+   public Class<?> defineClassX(String className, byte[] b, int off, int len) {
+     return super.defineClass(className, b, off, len);
+   }
+ }
